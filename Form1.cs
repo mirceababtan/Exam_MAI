056d694f3d57d19950e2b4ebda814f227f9e2c90
Mircea Babtan <LoL_Chiesd@yahoo.ro>
2023-01-29 17:59:50 +0200

    am adaugat problema 2 de la randul 1

--- Form1.cs
+++ Form1.cs
@@ -40,9 +40,9 @@ namespace Exam_MAI_r2_pb2
 
             for (int i = 0; i < n - 2; i++)
             {
-                for (int j = 0; j < n - 1; j++)
+                for (int j = i+1; j < n - 1; j++)
                 {
-                    for(int k=0;k<n;k++)
+                    for(int k=j+1;k<n;k++)
                     {
                         if (P_Max < perm(points[i], points[j], points[k]))
                         {
